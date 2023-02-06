98a701f97dee949704d8128095338da0ffa6576a
aichoudiallo <119548509+aichoudiallo@users.noreply.github.com>
2023-02-06 12:40:27 -0500

    weatherpy challenge completed

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "4618967a6d53b500ef1b5745a1af96b0"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "42ce9c75ec8f4467a288577e80f38cd5"
